248bfd68d808429f6e43a67bbcdfd40de414ea91
Prohor <prohor-yakovlev@mail.ru>
2022-12-10 16:47:53 +0300

    Unit test

--- src/01-hello-tests/01-01.test.ts
+++ src/01-hello-tests/01-01.test.ts
@@ -1,9 +1,16 @@
 import {mult, SplitIntoWords, sum} from "./01";
 
+let a: number
+let b: number
+let c: number
+
+beforeEach(()=> {
+    a = 1;
+    b = 2;
+    c = 3;
+})
+
 test('sum should be correct', () => {
-    const a = 1
-    const b = 2
-    const c = 3
 
     const result1 = sum(a, b)
     const result2 = sum(b, c)
@@ -11,10 +18,8 @@ test('sum should be correct', () => {
     expect(result1).toBe(3)
     expect(result2).toBe(5)
 })
+
 test('multiply should be correct', () => {
-    const a = 1
-    const b = 2
-    const c = 3
 
     const result1 = mult(a, b)
     const result2 = mult(b, c)
