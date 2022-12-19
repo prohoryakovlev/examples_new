4b7b1b9b035709d43df98c839d3911c4a987340c
Prohor <prohor-yakovlev@mail.ru>
2022-12-19 13:48:10 +0300

    Array map UnitTest

--- src/05/05_02.test.ts
+++ src/05/05_02.test.ts
@@ -1,5 +1,5 @@
 import {CityType} from "../02/02-02";
-import {getStreetsTitlesOfGovernmentBuildings} from "./05_02";
+import {createMessages, getStreetsTitlesOfGovernmentBuildings, getStreetsTitlesOfHouses} from "./05_02";
 
 let city: CityType;
 
@@ -72,11 +72,19 @@ test('list of streets titles of government buildings', ()=> {
 })
 
 //02. создайте в том же файле ещё одну функцию, чтобы тесты прошли
-test.skip('list of streets titles', ()=> {
+test('list of streets titles', ()=> {
     let streetsNames = getStreetsTitlesOfHouses(city.houses);
 
     expect(streetsNames.length).toBe(3);
     expect(streetsNames[0]).toBe("White street");
     expect(streetsNames[1]).toBe("Happy street");
     expect(streetsNames[2]).toBe("Happy street");
+})
+
+test("create greeting messages for street", ()=> {
+    let messages = createMessages(city.houses)
+    expect(messages.length).toBe(3)
+    expect(messages[0]).toBe("Hello guys from White street")
+    expect(messages[1]).toBe("Hello guys from Happy street")
+    expect(messages[2]).toBe("Hello guys from Happy street")
 })
--- src/05/05_02.ts
+++ src/05/05_02.ts
@@ -1,5 +1,14 @@
-import {GovernmentBuildingType} from "../02/02-02";
+import {GovernmentBuildingType, HouseType} from "../02/02-02";
 
 export const getStreetsTitlesOfGovernmentBuildings = (buildings: Array<GovernmentBuildingType>) => {
     return buildings.map(b => b.address.street.title)
+}
+
+
+export const getStreetsTitlesOfHouses = (houses: Array<HouseType>) => {
+    return houses.map(b => b.address.street.title)
+}
+
+export function createMessages(houses: Array<HouseType>) {
+    return houses.map(h => `Hello guys from ${h.address.street.title}`)
 }
