e5d1294f4a0539a2bd6d019151b7905f7fc59418
Prohor <prohor-yakovlev@mail.ru>
2022-12-19 14:07:10 +0300

    Array map UnitTest

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,20 @@ import React from 'react';
 import './App.css';
 
 function App() {
+    const names = ["Dimych", "Sveta", "Katya", "Victor", "Ignat"]
+
+    const users = [{name: "Dimych"}, {name: "Sveta"}, {name: "Katya"},{name: "Victor"}, {name: "Victor"}]
+
+    const liElements = users.map((u, index) => <li key={index}> - {u.name}</li>)
+
     return (
         <div className="App">
-
+            <ul>
+                {liElements}
+            </ul>
         </div>
     );
 }
-
 
 
 export default App;
