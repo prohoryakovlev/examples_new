907b94c1c8f65ee6168d69201560f413062f0e12
Prohor <prohor-yakovlev@mail.ru>
2023-01-04 15:10:33 +0300

    onChange

--- src/05/06-callbacks.tsx
+++ src/05/06-callbacks.tsx
@@ -6,27 +6,30 @@
 // window.setTimeout(callback, 1000)
 //
 
+import {ChangeEvent, MouseEvent} from "react";
+
 const User = () => {
 
-    const deleteUser = () => {
-        alert("user have been deleted")
+    const deleteUser = (event: MouseEvent<HTMLButtonElement>) => {
+        alert(event.currentTarget.name)
+        // alert("user have been deleted")
     }
-    const saveUser = () => {
-        alert("user have been saved")
-    }
-
     const onNameChanged = () => {
         console.log("name changed")
+    }
+    const onAgeChanged = (event: ChangeEvent<HTMLInputElement>) => {
+        console.log("age changed" + event.currentTarget.value)
     }
     const focusLostHandler = () => {
         console.log("focus lost")
     }
 
     return (
-        <div> <textarea onChange={onNameChanged} onBlur={focusLostHandler}>Dimych</textarea>
-            <input/>
-            <button onClick={deleteUser} >Delete</button>
-            <button onClick={saveUser}>Save</button>
+        <div>
+            <textarea onChange={onNameChanged} onBlur={focusLostHandler}>Dimych</textarea>
+            <input onChange={onAgeChanged} type={"number"}/>
+
+            <button name="delete" onClick={deleteUser} >delete</button>
         </div>
     )
 }
