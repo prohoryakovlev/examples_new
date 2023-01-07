6f93bf176135b481e2e0776d87a7c53e8fb28b79
Prohor <prohor-yakovlev@mail.ru>
2023-01-07 11:37:48 +0300

    Associative array

---
+++ src/08/08_01.test.ts
@@ -0,0 +1,13 @@
+import {userObj} from "./08_01";
+
+
+beforeEach(()=> {
+
+})
+
+test("should select corresponding user from obj", ()=> {
+    expect(userObj[0]).toBe('Dimych')
+    expect(userObj[1]).toBe('Natasha')
+    expect(userObj[2]).toBe('Valera')
+    expect(userObj[3]).toBe('Katya')
+})
---
+++ src/08/08_01.ts
@@ -0,0 +1,15 @@
+export const userArray = ['Dimych', 'Natasha', 'Valera', 'Katya']
+
+
+export const userObj = {
+    '0':'Dimych',
+    '1':'Natasha',
+    '2':'Valera',
+    '3':'Katya'
+}
+export const users = {
+    '101': {id: 101, name: 'Dimych'},
+    '3223':{id: 3223, name: 'Natasha'},
+    '1212':{id: 1212, name: 'Valera'},
+    '1':{id: 1, name: 'Katya'}
+}
