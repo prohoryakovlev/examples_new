cebc3b71c5a56606b0986d664b56aa965da3ab3f
Prohor <prohor-yakovlev@mail.ru>
2023-01-07 13:38:29 +0300

    Associative array test

--- src/08/08_01.test.ts
+++ src/08/08_01.test.ts
@@ -1,13 +1,25 @@
-import {userObj} from "./08_01";
 
 
-beforeEach(()=> {
+type UsersType = {
+    [key: string]: { id: number, name: string }
+}
+
+let users: UsersType
+
+
+beforeEach(() => {
+    users = {
+        '101': {id: 101, name: 'Dimych'},
+        '3223': {id: 3223, name: 'Natasha'},
+        '1212': {id: 1212, name: 'Valera'},
+        '1': {id: 1, name: 'Katya'}
+    }
 
 })
 
-test("should select corresponding user from obj", ()=> {
-    expect(userObj[0]).toBe('Dimych')
-    expect(userObj[1]).toBe('Natasha')
-    expect(userObj[2]).toBe('Valera')
-    expect(userObj[3]).toBe('Katya')
+test('should update corresponding user', () => {
+    users['1'].name = 'Ekaterina'
+
+    expect(users['1'].name).toBe('Ekaterina')
+
 })
